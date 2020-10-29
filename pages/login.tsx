1c7b70fa58dbd4ef4dac56ec6d22bd69d1e0aa97
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-10-29 19:03:10 +0100

    fix: login now uses a form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,21 +1,27 @@
-import {useState, useContext} from 'react'
-import AuthContext from '../contexts/AuthContext';
+import { useState, useContext } from "react";
+import AuthContext from "../contexts/AuthContext";
 
 export default function login() {
-  const [emailInput, setEmailInput] = useState<string>('');
-  const {login} = useContext(AuthContext);
+  const [emailInput, setEmailInput] = useState<string>("");
+  const { login } = useContext(AuthContext);
 
-  const loginHandler = () => {
-    console.log('logging in', emailInput);
+  const loginHandler = (e) => {
+    console.log("logging in", emailInput);
+    e.preventDefault();
     login(emailInput);
-  }
-
+  };
 
   return (
     <div>
       login page
-      <input type="email" required onChange={(e) => setEmailInput(e.target.value)} />
-      <button type="button" onClick={loginHandler}>Login</button>
+      <form onSubmit={loginHandler}>
+        <input
+          type="email"
+          required
+          onChange={(e) => setEmailInput(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
-  )
+  );
 }

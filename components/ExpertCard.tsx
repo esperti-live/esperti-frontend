846b9b50ac141b34f154a4c59a2e151b83b99acd
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-10-31 12:25:10 +0100

    feat: expert card has description

--- components/ExpertCard.tsx
+++ components/ExpertCard.tsx
@@ -2,15 +2,20 @@ import styles from "../styles/Expert.module.scss";
 
 export default function Card({ image_url, name, title, skills }) {
   return (
-    <div className={styles.expertCard}>
+    <article className={styles.expertCard}>
       <img src={image_url} alt={name} />
       <h5 className={styles.expertName}>{name}</h5>
       <span className="title">{title}</span>
       <div className={styles.tags}>
-        {skills.map((skill) => (
+        {skills.slice(0, 3).map((skill) => (
           <span key={skill}>{skill}</span>
         ))}
       </div>
-    </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis labore
+        quam deserunt, voluptatum illo quaerat! Provident minima delectus
+        perferendis rem.
+      </p>
+    </article>
   );
 }

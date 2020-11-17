a72860ccfd4a29e0b119c11f5e6ad7b20ceefaa4
NikVogri <55229210+NikVogri@users.noreply.github.com>
2020-11-17 20:59:05 +0100

    feat: user can create review for session

--- pages/sessions/review/[slug].tsx
+++ pages/sessions/review/[slug].tsx
@@ -18,7 +18,7 @@ const ReviewAndPay = () => {
     const data = {
       rating,
       comment: textarea,
-      session_id: session.id,
+      sessionId: session.id,
     };
 
     axios.post(`${process.env.NEXT_PUBLIC_STRAPI_URL}/reviews`, data, {
@@ -26,45 +26,43 @@ const ReviewAndPay = () => {
     });
   };
 
-  useEffect(() => {
-    console.log(session);
-  }, []);
-
-  if (!user.tokenId) {
+  if (!session) {
+    return <p>Session could not be found</p>;
+  } else if (!user || !user.tokenId) {
     return <p>Loading...</p>;
-  }
-
-  return (
-    <section className={styles.review}>
-      <h1>Review and Pay</h1>
-      <div className={styles.info}>
-        <p>{session.expertName}</p>
-        <p>{session.totalTime} minutes</p>
-        <p>
-          <strong>${session.paymentTotal}</strong>
-        </p>
-      </div>
-      <div className={styles.rate}>
-        <div className={styles.starRating}>
-          <StarRating
-            rating={rating}
-            updateHandler={(rating: number) => setRating(rating)}
-          />
+  } else {
+    return (
+      <section className={styles.review}>
+        <h1>Review and Pay</h1>
+        <div className={styles.info}>
+          <p>{session.expertName}</p>
+          <p>{session.totalTime} minutes</p>
+          <p>
+            <strong>${session.paymentTotal}</strong>
+          </p>
         </div>
-        <form onSubmit={formSubmitHandler}>
-          <textarea
-            className="editInput"
-            rows={5}
-            onChange={(e) => setTextarea(e.target.value)}
-            value={textarea}
-          ></textarea>
-          <button type="submit" className={styles.reviewButton}>
-            Confirm & Pay
-          </button>
-        </form>
-      </div>
-    </section>
-  );
+        <div className={styles.rate}>
+          <div className={styles.starRating}>
+            <StarRating
+              rating={rating}
+              updateHandler={(rating: number) => setRating(rating)}
+            />
+          </div>
+          <form onSubmit={formSubmitHandler}>
+            <textarea
+              className="editInput"
+              rows={5}
+              onChange={(e) => setTextarea(e.target.value)}
+              value={textarea}
+            ></textarea>
+            <button type="submit" className={styles.reviewButton}>
+              Confirm & Pay
+            </button>
+          </form>
+        </div>
+      </section>
+    );
+  }
 };
 
 export default ReviewAndPay;
